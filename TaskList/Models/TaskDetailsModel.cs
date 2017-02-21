6be9d4b4be7778c7727f98b083be167ba3038c38
KevStrong <kev.strong@ntlworld.com>
2017-02-21 17:39:04 +0000

    Ading project to GitHub

---
+++ TaskList/Models/TaskDetailsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TaskList.Models
+{
+    public class TaskDetailsModel
+    {
+        public int ID { get; set;}
+        
+        [AllowHtml]
+        [Required(ErrorMessage="Please specify a task description before submitting again")]
+        [DisplayName("Task")]
+        public string TaskDescription { get; set; }
+
+        [DisplayName("Completed")]
+        public bool TaskCompleted { get; set; }
+    }
+
+    
+    public class TaskListModel : TaskDetailsModel
+    {
+        public List<TaskDetailsModel> Tasks { get; set; }
+    }   
+}
