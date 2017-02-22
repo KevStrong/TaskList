e4b388d662154b232d3ae616e891ca417b5a549a
KevStrong <kev.strong@ntlworld.com>
2017-02-22 22:30:55 +0000

    Added functionality for completed tasks
    Strikesthrough text of task when check box is checked.  Controlled by
    bundled JS file and new CSS style.

---
+++ TaskList/Scripts/tasklist-completedtask.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("input[type=checkbox]").click(function () {
+        var ElementId = $(this).attr("checkfor");
+        var pretext = document.getElementById(ElementId);
+
+        var attr = $(pretext).attr('class');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $(pretext).removeAttr("class"); 
+        }
+        else {
+            $(pretext).attr("class", "strikeout");
+        }
+    });
+});
